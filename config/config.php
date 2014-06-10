3e9d8d683f67023e61ec51d8787ff3d6663fb762
johantan <ztan@findthebest.com>
2014-06-10 01:23:22 -0700

    Added connection from production web servers

--- config/config.php
+++ config/config.php
@@ -1,35 +1,35 @@
 <?php
 /**
- * Default configuration for Xhgui
+* Default configuration for Xhgui
  */
 return array(
-    'debug' => false,
-    'mode' => 'development',
+		'debug' => false,
+		'mode' => 'development',
 
-    // Can be either mongodb or file.
-    'save.handler' => 'mongodb',
+		// Can be either mongodb or file.
+		'save.handler' => 'mongodb',
 
-    // Needed for file save handler. Beware of file locking. You can adujst this file path 
-    // to reduce locking problems (eg uniqid, time ...)
-    //'save.handler.filename' => __DIR__.'/../data/xhgui_'.date('Ymd').'.dat',
-    'db.host' => 'mongodb://127.0.0.1:27017',
-    'db.db' => 'xhprof',
+		// Needed for file save handler. Beware of file locking. You can adujst this file path
+		// to reduce locking problems (eg uniqid, time ...)
+		//'save.handler.filename' => __DIR__.'/../data/xhgui_'.date('Ymd').'.dat',
+		'db.host' => (exec('whoami') == 'www-data') ? 'mongodb://10.174.77.212:27017' : 'mongodb://127.0.0.1:27017',
+		'db.db' => 'xhprof',
 
-    // Allows you to pass additional options like replicaSet to MongoClient.
-    'db.options' => array(),
-    'templates.path' => dirname(__DIR__) . '/src/templates',
-    'date.format' => 'M jS H:i:s',
-    'detail.count' => 6,
-    'page.limit' => 25,
+		// Allows you to pass additional options like replicaSet to MongoClient.
+		'db.options' => array(),
+		'templates.path' => dirname(__DIR__) . '/src/templates',
+		'date.format' => 'M jS H:i:s',
+		'detail.count' => 6,
+		'page.limit' => 25,
 
-    // Profile 1 in 100 requests.
-    // You can return true to profile every request.
-    'profiler.enable' => function() {
-        return $_GET['xprofile'] == 1;
-    },
+		// Profile 1 in 100 requests.
+		// You can return true to profile every request.
+		'profiler.enable' => function() {
+			return $_GET['xprofile'] == 1;
+		},
 
-    'profiler.simple_url' => function($url) {
-        return $url;
-    }
+		'profiler.simple_url' => function($url) {
+			return $url;
+		}
 
 );
