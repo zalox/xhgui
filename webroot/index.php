8b13936fe1a8ae52281ed090217df3a825be94d9
johantan <ztan@findthebest.com>
2016-03-17 16:05:35 -0700

    Use mongofill, fix php7 compatibility

--- webroot/index.php
+++ webroot/index.php
@@ -1,6 +1,16 @@
 <?php
 require dirname(__DIR__) . '/src/bootstrap.php';
 
+spl_autoload_register(function ($class_name) {
+	$class_name = str_replace('Mongofill\\', 'Mongofill/', $class_name);
+	if (file_exists("../../mongofill/src/{$class_name}.php")) {
+		require_once "../../mongofill/src/{$class_name}.php";
+		require_once "../../mongofill/src/functions.php";
+		return true;
+	}
+	return false;
+});
+
 $di = new Xhgui_ServiceContainer();
 
 $app = $di['app'];
